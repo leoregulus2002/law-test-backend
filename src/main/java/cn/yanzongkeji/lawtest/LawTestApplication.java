342a02c366977d330e918788a9c95aa1554e974c
Di Xiao Yu <leoregulus2002@outlook.com>
2026-09-02 11:21:02 +0800

    feature: init

---
+++ src/main/java/cn/yanzongkeji/lawtest/LawTestApplication.java
@@ -0,0 +1,13 @@
+package cn.yanzongkeji.lawtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yanzongkeji/lawtest/LawTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yanzongkeji.lawtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
